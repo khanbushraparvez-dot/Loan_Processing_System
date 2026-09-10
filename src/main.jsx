c54ca85b7795bfea60fc9c0ac1080137ceb55aa4
Khan Bushra <khanbushraparvez@gmail.com>
2026-09-10 11:07:18 +0530

    Initialize React application with App component

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "../LoanSystem-1.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <App />
+);
